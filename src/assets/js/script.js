e928e12f9edd6c0d75c44252347cd5a00234a50d
Dhinesh <dhinesh.tech2001@gmail.com>
2024-02-14 12:50:01 +0530

    About title & Responsive Added

---
+++ src/assets/js/script.js
@@ -0,0 +1,6 @@
+// BG SCROLLING ANIMATION
+/* Set width of all animated text to match container */
+let parent = document.querySelectorAll(".animate-text");
+for (let i = 0; i < parent.length; i++) {
+  parent[i].style.width = parent[i].children[0].clientWidth + "px";
+}
